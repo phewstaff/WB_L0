3cb691335c838f0a2ee405048c1e14a6a601ece4
magomed_n <naurbaev2020@gmail.com>
2023-11-15 16:23:34 +0300

    extract card render

---
+++ js/components/CardRenderer.js
@@ -0,0 +1,37 @@
+export function renderCard(data) {
+  const cardContainer = document.getElementById("card-container");
+  const template = document.getElementById("card-template");
+
+  data.forEach((item) => {
+    const card = template.cloneNode(true);
+    card.removeAttribute("id"); // Removed the id attribute from the cloned card
+    card.removeAttribute("style");
+
+    card.querySelector("#item-checkbox").id = item.id;
+    card.querySelector("label").htmlFor = item.id;
+
+    card.querySelector("#price-md").textContent = `${item.price.toLocaleString(
+      "ru-RU"
+    )} сом`;
+    card.querySelector(
+      "#discount-md"
+    ).textContent = `${item.discountPrice.toLocaleString("ru-RU")} сом`;
+    card.querySelector("#price-sm").textContent =
+      item.price.toLocaleString("ru-RU");
+    card.querySelector(
+      "#discount-sm"
+    ).textContent = `${item.discountPrice.toLocaleString("ru-RU")} сом`;
+
+    card.querySelector("#item-image").src = item.imageUrl;
+    card.querySelector("#item-name").textContent = item.name;
+    card.querySelector("#item-color").textContent = `Цвет: ${item.color}`;
+    card.querySelector("#item-brand").textContent = item.brand;
+    card.querySelector(
+      "#remaining"
+    ).textContent = `Осталось ${item.remaining} шт.`;
+
+    card.querySelector("#quantity").textContent = item.quantity;
+
+    cardContainer.appendChild(card); //adding card to dom
+  });
+}
